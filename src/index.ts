93073984c79c406f0c59d8bb2dc3473dfced743e
Alistair Chapman <alistair@agchapman.com>
2021-06-08 23:46:39 +1000

    Minor docs update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * To get started reading and parsing a PAK file, create a new {@link PakFileReader} instance as outlined in the README.
+ * 
+ * The parsers used by {@link PakFileReader} (created with [binary-parser](https://www.npmjs.com/package/binary-parser)) are available (for low-level usage) in the {@link ParserService}.
+ * 
+ * @module
+ */
 export * from './fileReader';
 export * from './types';
 export {ParserService} from './parser'
